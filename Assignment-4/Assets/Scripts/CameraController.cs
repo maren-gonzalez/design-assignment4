335c98f0d0a24012a234f5e560f59c961fbd94d3
Maren <113512118+maren-gonzalez@users.noreply.github.com>
2024-10-03 11:02:28 +0200

    updated camera movement

--- Assignment-4/Assets/Scripts/CameraController.cs
+++ Assignment-4/Assets/Scripts/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour
 {
     private CharacterController characterController;
     public float speed;
+    public float rotationSpeed;
     
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,13 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.Move(movement * (speed * Time.deltaTime));
+        
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * vertical * speed * Time.deltaTime);
+
+        //get turning input + transform position
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * horizontal * rotationSpeed * Time.deltaTime);
+     
     }
 }
