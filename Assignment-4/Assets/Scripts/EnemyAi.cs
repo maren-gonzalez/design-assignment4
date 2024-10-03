2792889eca63daa0ab5a05d4d2dbc01acc38713b
Jake Madlon <jjd.madlon@student.han.nl>
2024-10-03 10:52:15 +0200

    Made enemy behaviour

---
+++ Assignment-4/Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyAi : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent agent;
+
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private LayerMask Ground, Player;
+
+    //Patrolling
+    [SerializeField]
+    private Vector3 walkPoint;
+    private bool walkPointSet;
+    [SerializeField]
+    private float walkPointRange;
+
+    //States
+    [SerializeField]
+    private float sightRange, attackRange;
+    [SerializeField]
+    private bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, Player);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, Player);
+
+        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if(playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if(walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if(distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+        float randomX = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, Ground))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+    }
+}
